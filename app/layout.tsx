48f3eb05eb925cbfdb79a233a6b1f9dfec6d2332
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-01-23 15:20:38 +0300

    fix: :bug: Update csp middleware and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,25 +21,36 @@ export const metadata: Metadata = {
 	},
 	description:
 		"Professional Front-End Developer specializing in React, Next.js, and TypeScript, delivering high-performance, responsive, and SEO-optimized websites. With expertise in modern tools like Tanstack Query, Zustand, and Webpack, I craft scalable solutions tailored to your business needs. From dynamic web applications to user-friendly interfaces, I focus on quality, security, and seamless user experiences. Let's bring your vision to life—partner with me for reliable and innovative web development solutions!",
-	generator: 'Next.js',
-	applicationName: 'Ayberk Yavas',
-	referrer: 'origin-when-cross-origin',
-	keywords: ['Front-End Developer', 'Ayberk Yavas', 'Websitesi Yaptirmak', 'Next.js', 'React', 'JavaScript', 'TypeScript', 'CSS', 'HTML'],
-	authors: { name: 'Ayberk Yavas', url: 'https://ayberkyavas.com' },
-	creator: 'Ayberk Yavas',
-	publisher: 'Ayberk Yavas',
+	generator: "Next.js",
+	applicationName: "Ayberk Yavas",
+	referrer: "origin-when-cross-origin",
+	keywords: [
+		"Front-End Developer",
+		"Ayberk Yavas",
+		"Websitesi Yaptirmak",
+		"Next.js",
+		"React",
+		"JavaScript",
+		"TypeScript",
+		"CSS",
+		"HTML",
+	],
+	authors: { name: "Ayberk Yavas", url: "https://ayberkyavas.com" },
+	creator: "Ayberk Yavas",
+	publisher: "Ayberk Yavas",
 	formatDetection: {
 		email: false,
 		address: false,
 		telephone: false,
 	},
 	openGraph: {
-		type: 'website',
-		locale: 'en_US',
-		url: 'https://ayberkyavas.com',
-		siteName: 'Ayberk Yavas',
-		title: 'Ayberk Yavas',
-		description: "Professional Front-End Developer specializing in React, Next.js, and TypeScript, delivering high-performance, responsive, and SEO-optimized websites. With expertise in modern tools like Tanstack Query, Zustand, and Webpack, I craft scalable solutions tailored to your business needs. From dynamic web applications to user-friendly interfaces, I focus on quality, security, and seamless user experiences. Let's bring your vision to life—partner with me for reliable and innovative web development solutions!",
+		type: "website",
+		locale: "en_US",
+		url: "https://ayberkyavas.com",
+		siteName: "Ayberk Yavas",
+		title: "Ayberk Yavas",
+		description:
+			"Professional Front-End Developer specializing in React, Next.js, and TypeScript, delivering high-performance, responsive, and SEO-optimized websites. With expertise in modern tools like Tanstack Query, Zustand, and Webpack, I craft scalable solutions tailored to your business needs. From dynamic web applications to user-friendly interfaces, I focus on quality, security, and seamless user experiences. Let's bring your vision to life—partner with me for reliable and innovative web development solutions!",
 	},
 };
 
@@ -49,7 +60,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en' className="scroll-smooth">
+		<html lang='en' className='scroll-smooth'>
 			<body className={`${outfit.variable} ${inter.variable} antialiased`}>
 				{children}
 			</body>
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-
+	// const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 	const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: 'unsafe-eval' https://cdn.sanity.io https://js.sentry-cdn.com https://browser.sentry-cdn.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: https://browser.sentry-cdn.com https://js.sentry-cdn.com;
     style-src 'self' 'unsafe-inline' https://cdn.sanity.io;
-    img-src 'self' blob: data: https://cdn.sanity.io https://hostedscan.com/;
-    connect-src 'self' https://*.sanity.io https://sentry.io https://o*.ingest.sentry.io;
-    font-src 'self' https://cdn.sanity.io;
+    img-src 'self' blob: data: https://cdn.sanity.io https://hostedscan.com/protected-by-hostedscan.svg;
+    connect-src 'self' *.sanity.io *.sentry.io;
+    font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
@@ -21,12 +20,12 @@ export function middleware(request: NextRequest) {
 		.trim();
 
 	const requestHeaders = new Headers(request.headers);
-	requestHeaders.set("x-nonce", nonce);
+	// requestHeaders.set("x-nonce", nonce);
 	requestHeaders.set(
 		"Content-Security-Policy",
 		contentSecurityPolicyHeaderValue
 	);
-  request.headers.set("x-sentry-csp-report-only", "true");
+	requestHeaders.set("x-sentry-csp-report-only", "true");
 
 	const response = NextResponse.next({
 		request: {
