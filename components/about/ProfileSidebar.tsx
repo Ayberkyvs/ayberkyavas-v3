cbc62392d90816398e83c74cc81b6cacaf7cf9a2
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-01-24 22:51:59 +0300

    feat: :sparkles: Add hire status badge

--- components/about/ProfileSidebar.tsx
+++ components/about/ProfileSidebar.tsx
@@ -1,22 +1,31 @@
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
-import { EarthIcon } from "lucide-react";
+import { Dot, EarthIcon } from "lucide-react";
 import { AboutMe } from "@/sanity/types";
 import { urlFor } from "@/sanity/lib/image";
 
-export default function ProfileSidebar({data}: { data: Pick<AboutMe, "imageSrc" | "location"> }) {
+export default function ProfileSidebar({
+	data,
+}: {
+	data: Pick<AboutMe, "imageSrc" | "location">;
+}) {
 	const { imageSrc, location } = data;
 	return (
-		<div className='sticky top-8 flex sm:flex-col items-center gap-5'>
-			<Image
-				src={urlFor(imageSrc).width(204).url()}
-				alt='Ayberk Yavas Resume Picture'
-				width={204}
-				height={204}
-				className='rounded-full max-sm:w-[120px] max-sm:h-[120px]'
-				priority
-			/>
-			<div className='flex justify-start items-center md:flex-center flex-col gap-5'>
+		<div className='sticky top-8 flex sm:flex-col items-center gap-5 flex-wrap'>
+			<div className='relative'>
+				<Image
+					src={urlFor(imageSrc).width(204).url()}
+					alt='Ayberk Yavas Resume Picture'
+					width={204}
+					height={204}
+					className='rounded-full max-sm:w-[120px] max-sm:h-[120px]'
+					priority
+				/>
+				<Badge className='absolute bottom-0 left-1/2 -translate-x-1/2  bg-green-300 border border-green-900 w-fit text-center text-xs md:text-sm truncate text-green-900'>
+					Available for a hire!
+				</Badge>
+			</div>
+			<div className='flex justify-start items-center md:flex-center flex-col gap-2'>
 				<p className='flex paragraph gap-[5px] items-center'>
 					<EarthIcon className='w-4 h-4' />
 					{location}
