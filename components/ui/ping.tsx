40f24f65369bf77042a77c55fe2a81fb7783507e
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-11-03 10:44:22 +0100

    feat(ui): Add new UI components and theme provider

--- components/ui/ping.tsx
+++ components/ui/ping.tsx
@@ -1,34 +1,34 @@
 import { cn } from "@/lib/utils";
 
 interface PingProps {
-	isAvailable: boolean;
-	colors: { success: string; error: string };
-	className?: string;
+  isAvailable: boolean;
+  colors: { success: string; error: string };
+  className?: string;
 }
 const Ping = ({ isAvailable, colors, className = "" }: PingProps) => {
-	const { success, error } = colors;
-	return (
-		<>
-			<div className={cn("relative", className)}>
-				<div className="">
-					<span className="flex size-[11px]">
-						<span
-							className={cn(
-								"absolute inline-flex size-full animate-ping rounded-full opacity-75",
-								isAvailable ? success : error
-							)}
-						></span>
-						<span
-							className={cn(
-								"relative inline-flex size-[11px] rounded-full",
-								isAvailable ? success : error
-							)}
-						></span>
-					</span>
-				</div>
-			</div>
-		</>
-	);
+  const { success, error } = colors;
+  return (
+    <>
+      <div className={cn("relative", className)}>
+        <div className="size-full">
+          <span className="flex size-full">
+            <span
+              className={cn(
+                "absolute inline-flex size-full animate-ping rounded-full opacity-75",
+                isAvailable ? success : error,
+              )}
+            ></span>
+            <span
+              className={cn(
+                "relative inline-flex size-full rounded-full",
+                isAvailable ? success : error,
+              )}
+            ></span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Ping;
--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -1,33 +1,51 @@
 import React, { ReactNode } from "react";
+import { Badge } from "./badge";
 
 interface SectionProps {
-	children: ReactNode;
-	className?: string;
+  children: ReactNode;
+  className?: string;
 }
 
 interface SectionChildProps {
-	children: ReactNode;
-	className?: string;
+  children: ReactNode;
+  className?: string;
 }
 
 const Section: React.FC<SectionProps> & {
-	SubHeading: React.FC<SectionChildProps>;
-	Heading: React.FC<SectionChildProps>;
-	Description: React.FC<SectionChildProps>;
+  SubHeading: React.FC<SectionChildProps>;
+  Heading: React.FC<SectionChildProps>;
+  Description: React.FC<SectionChildProps>;
+  Badge: React.FC<SectionChildProps>;
 } = ({ children, className = "" }) => {
-	return <div className={`section ${className}`}>{children}</div>;
+  return <div className={`section ${className}`}>{children}</div>;
 };
 
 Section.SubHeading = ({ children, className = "" }) => {
-	return <span className={`section_sub-heading ${className}`}>{children}</span>;
+  return (
+    <span className={`section_sub-heading text-balance ${className}`}>
+      {children}
+    </span>
+  );
 };
 
 Section.Heading = ({ children, className = "" }) => {
-	return <h2 className={`section_heading ${className}`}>{children}</h2>;
+  return (
+    <h2 className={`section_heading text-balance ${className}`}>{children}</h2>
+  );
 };
 
 Section.Description = ({ children, className = "" }) => {
-	return <p className={`section_paragraph ${className}`}>{children}</p>;
+  return (
+    <p className={`section_paragraph text-balance ${className}`}>{children}</p>
+  );
+};
+
+Section.Badge = ({ children, className = "" }) => {
+  return (
+    <Badge variant="secondary" className={`mb-4 w-fit ${className}`}>
+      {children}
+    </Badge>
+  );
 };
 
 export default Section;
